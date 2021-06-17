9c2a42a24410658e8198a5272741624840168dd9
Bukola Omojowo <bomojowo@gmail.com>
2021-06-17 12:55:50 +0100

    Formatted using prettier

--- src/components/FriendStatuses.tsx
+++ src/components/FriendStatuses.tsx
@@ -30,64 +30,66 @@ function FriendStatuses(): JSX.Element {
           timeAgo={{ n: 19, unit: "minutes" }}
         />
         <StatusItem
-        username={"drx"}
-        content={"chatting in gmail w/Jack"}
-        emoji={"⭐"}
-        timeAgo={{ n:31, unit: "minutes" }}
+          username={"drx"}
+          content={"chatting in gmail w/Jack"}
+          emoji={"⭐"}
+          timeAgo={{ n: 31, unit: "minutes" }}
         />
         <StatusItem
-        username={"biz"}
-        content={"having some coffee"}
-        emoji={"⭐"}
-        timeAgo={{ n:36, unit: "minutes" }}
+          username={"biz"}
+          content={"having some coffee"}
+          emoji={"⭐"}
+          timeAgo={{ n: 36, unit: "minutes" }}
         />
         <StatusItem
-        username={"Florian"}
-        content={"Preparing a pizza"}
-        emoji={"⭐"}
-        timeAgo={{ n:3, unit: "hours" }}
+          username={"Florian"}
+          content={"Preparing a pizza"}
+          emoji={"⭐"}
+          timeAgo={{ n: 3, unit: "hours" }}
         />
         <StatusItem
-        username={"Courtney"}
-        content={"multi-tasking audio debug audio coding"}
-        emoji={"⭐"}
-        timeAgo={{ n:8, unit: "hours" }}
-        />
-       <StatusItem
-        username={"Jack"}
-        content={"fantasizing about jack drawing naked people mmmmmmmmmmmmm..... naked people."}
-        emoji={"⭐"}
-        timeAgo={{ n:21, unit: "hours" }}
+          username={"Courtney"}
+          content={"multi-tasking audio debug audio coding"}
+          emoji={"⭐"}
+          timeAgo={{ n: 8, unit: "hours" }}
         />
         <StatusItem
-        username={"noah"}
-        content={"Oh crap, I think I might be getting that f'in cold"}
-        emoji={"⭐"}
-        timeAgo={{ n:21, unit: "hours" }}
-        />
-       <StatusItem
-        username={"asruge"}
-        content={"put some rss on my mp3"}
-        emoji={"⭐"}
-        timeAgo={{ n:22, unit: "hours" }}
-        />
-         <StatusItem
-        username={"crystal"}
-        content={"in the musicals"}
-        emoji={"⭐"}
-        timeAgo={{ n:1, unit: "days" }}
+          username={"Jack"}
+          content={
+            "fantasizing about jack drawing naked people mmmmmmmmmmmmm..... naked people."
+          }
+          emoji={"⭐"}
+          timeAgo={{ n: 21, unit: "hours" }}
         />
         <StatusItem
-        username={"Tim Roberts"}
-        content={"setting up my mac mini"}
-        emoji={"◻️"}
-        timeAgo={{ n:1, unit: "days" }}
+          username={"noah"}
+          content={"Oh crap, I think I might be getting that f'in cold"}
+          emoji={"⭐"}
+          timeAgo={{ n: 21, unit: "hours" }}
         />
         <StatusItem
-        username={"4153738157"}
-        content={"just setting up my twttr"}
-        emoji={"◻️"}
-        timeAgo={{ n:1, unit: "days" }}
+          username={"asruge"}
+          content={"put some rss on my mp3"}
+          emoji={"⭐"}
+          timeAgo={{ n: 22, unit: "hours" }}
+        />
+        <StatusItem
+          username={"crystal"}
+          content={"in the musicals"}
+          emoji={"⭐"}
+          timeAgo={{ n: 1, unit: "days" }}
+        />
+        <StatusItem
+          username={"Tim Roberts"}
+          content={"setting up my mac mini"}
+          emoji={"◻️"}
+          timeAgo={{ n: 1, unit: "days" }}
+        />
+        <StatusItem
+          username={"4153738157"}
+          content={"just setting up my twttr"}
+          emoji={"◻️"}
+          timeAgo={{ n: 1, unit: "days" }}
         />
       </ol>
     </section>
